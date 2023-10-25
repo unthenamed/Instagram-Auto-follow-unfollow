8f4715ae4927b84d227ff620324c0bb53ba0cdbe
unthenamed <unthenamed@gmail.com>
2023-10-25 20:50:19 +0700

    up

---
+++ suka.py
@@ -0,0 +1,46 @@
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+from time import sleep
+import pickle
+import os
+SESSION_FILE = 'cookies.pkl'
+chrome_service = Service(ChromeDriverManager().install())
+chrome_options = Options()
+options = [
+#    "--headless",
+    "--disable-gpu",
+    "--window-size=360,640",
+    "--ignore-certificate-errors",
+    "--disable-extensions",
+    "--no-sandbox",
+    "--disable-dev-shm-usage",
+    "--user-agent=Mozilla/5.0 (iPhone; CPU iPhone OS 10_3 like Mac OS X) AppleWebKit/602.1.50 (KHTML, like Gecko) CriOS/56.0.2924.75 Mobile/14E5239e Safari/602.1"
+]
+for option in options:
+    chrome_options.add_argument(option)
+webdriver = webdriver.Chrome(service=chrome_service, options=chrome_options)
+sleep(5)
+
+def tekan():
+    WebDriverWait(webdriver, 120).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="react-root"]/div[3]/div[1]/div[1]/span/div/div/span/svg'))).click()    
+
+def start() :
+    if os.path.exists(SESSION_FILE):
+        webdriver.get('https://www.instagram.com/accounts/login/')
+        cookies = pickle.load(open("cookies.pkl", "rb"))
+        for cookie in cookies:
+            webdriver.add_cookie(cookie)
+        webdriver.get('https://www.instagram.com/reels/')
+    else :
+        pass
+
+love = '//*[@id="react-root"]/div[3]/div[1]/div[1]/span/div/div/span/svg'
+
+start()
+tekan()
+sleep(30)
